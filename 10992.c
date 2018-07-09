e4ac4011927f49c9e73f041ba7e94c39b963fce3
ParkHaeSeon <35719365+ParkHaeSeon@users.noreply.github.com>
2018-07-09 22:08:04 +0900

    별 찍기 - 17
    별 찍기 - 17

---
+++ 10992.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n = 0, c=0;
+	int var = 0;
+	int i, j, k, h;
+
+	scanf("%d", &n);
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = i; j < n; j++)
+		{
+			printf(" ");
+		}
+
+		if (i != n)
+		{
+			for (k = 1; k <= 2 * i - 1; k++)
+			{
+				if (k == 1 || k == 2 * i - 1) printf("*");
+				else printf(" ");
+			}
+		}
+		else
+		{
+
+			for (h = 0; h < 2 * n - 1; h++)
+			{
+				printf("*");
+			}
+		}
+
+		printf("\n");
+	}
+
+	printf("\n");
+	
+	return 0;
+}
