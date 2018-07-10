56fb0525d273eabdd9a21c3389a4cafd36096ee6
ParkHaeSeon <35719365+ParkHaeSeon@users.noreply.github.com>
2018-07-11 00:33:37 +0900

    부녀회장이 될테야
    2차원 배열 [층][호]

---
+++ 2775.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+int T = 0; // 테스트 케이스
+int k = 0; // 층
+int n = 0; // 호
+
+int cnt[15][15] = { 0, };
+
+void people(int floor, int number)
+{
+	for (int f = 1; f <= floor; f++)
+	{
+		for (int f_n = 1; f_n <= number; f_n++)
+		{
+			cnt[f][f_n] = cnt[f][f_n - 1] + cnt[f - 1][f_n];
+		}
+	}
+}
+
+int main(void)
+{
+	for (int i = 1; i <= 14; i++)
+	{
+		cnt[0][i] = i;
+	}
+
+	scanf("%d", &T);
+
+	while (T > 0)
+	{
+		scanf("%d", &k);
+
+		scanf("%d", &n);
+		
+		people(k, n);
+
+		printf("%d\n", cnt[k][n]);
+
+		--T;
+	}
+
+	return 0;
+}
