faf26c32c0fc0a8746bbeea6c6c012149ec7056c
ParkHaeSeon <35719365+ParkHaeSeon@users.noreply.github.com>
2018-07-12 23:05:07 +0900

    알파벳 찾기
    getline(cin, s)로 해야된다. 그냥 scanf("%s", s)로 하면 런타임 에러.

---
+++ 10809.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+string s;
+
+int main(void)
+{
+	int pos = 0;
+
+	getline(cin, s);
+
+	for (int i = 'a'; i <= 'z'; i++)
+	{
+		auto it = find(s.begin(), s.end(), i);
+
+		if (it == s.end()) printf("-1 ");
+		else cout << it-s.begin() << " ";
+	}
+
+	printf("\n");
+
+	return 0;
+}
