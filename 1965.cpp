23f9aa6a0ec55121356c90a1733e0761743699c4
ParkHaeSeon <35719365+ParkHaeSeon@users.noreply.github.com>
2018-07-17 08:59:55 +0900

    상자 넣기(가장 긴 증가 부분 수열)
    상자 넣기(가장 긴 증가 부분 수열)

---
+++ 1965.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+int N = 0; // 수열의 크기
+int num[1001] = { 0, };
+int dp[1001] = { 0, };
+
+using namespace std;
+
+int main(void)
+{
+	int max = 0;
+	scanf("%d", &N);
+
+	for (int i = 1; i <= N; i++)
+	{
+		scanf("%d", &num[i]);
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		dp[i] = 1; // 길이
+
+		for (int j = 1; j < i; j++) // 그 전의 것들을 하나씩 다 비교. i-1까지.
+		{
+			if (num[i] > num[j] && dp[j] + 1 > dp[i])
+			{
+				dp[i] = dp[j] + 1;
+			}
+		}
+
+		if (max < dp[i]) max = dp[i];
+	}
+
+	printf("%d\n", max);
+
+	return 0;
+}
