32ff2498ba9443a9e0c4f3ba7f31d245654a5c58
ParkHaeSeon <35719365+ParkHaeSeon@users.noreply.github.com>
2018-07-27 13:47:18 +0900

    동물원
    동물원

---
+++ 1309.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cstdlib>
+#include <queue>
+
+using namespace std;
+
+int N = 0;
+
+long long dp[100001][3] = { 0, };
+
+long long animal[100001][3] = { 0, };
+
+int main(void)
+{
+	long long Sum = 0;
+
+	scanf("%d", &N);
+
+
+	for (int i = 0; i <= 2; i++)
+	{
+		dp[1][i] = 1;
+	}
+
+	for (int i = 2; i <= N; i++)
+	{
+		dp[i][0] = dp[i - 1][0] % 9901 + dp[i - 1][1] % 9901 + dp[i - 1][2] % 9901;
+
+		dp[i][1] = dp[i - 1][0] % 9901 + dp[i - 1][2] % 9901;
+
+		dp[i][2] = dp[i - 1][0] % 9901 + dp[i - 1][1] % 9901;
+	}
+
+	Sum = dp[N][0] % 9901 + dp[N][1] % 9901 + dp[N][2] % 9901;
+
+	printf("%d\n", Sum % 9901);
+
+	return 0;
+}
