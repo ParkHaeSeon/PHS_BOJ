ea5734d3b321dc2277ac85193143d760998eb5cc
ParkHaeSeon <35719365+ParkHaeSeon@users.noreply.github.com>
2018-07-29 00:46:56 +0900

    이동하기
    이동하기

--- 11048.c
+++ 11048.c
@@ -1,41 +1,40 @@
-#include <stdio.h>
-#include <malloc.h>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <vector>
 
-int room[1001][1001];
-int Dp[1001][1001];
+using namespace std;
 
-int max(int a, int b)
-{
-	if (a >= b) return a;
-	else return b;
-}
+int room[1001][1001] = { 0, };
+int dp[1001][1001] = { 0, };
+
+int N = 0; // 세로 = y
+int M = 0; // 가로 = x
 
 int main(void)
 {
-	int N, M, i, j, Max=0;
-
 	scanf("%d %d", &N, &M);
 
-
-	for (i = 1; i <= N; i++)
+	for (int i = 1; i <= N; i++)
 	{
-		for (j = 1; j <= M; j++)
+		for (int j = 1; j <= M; j++)
 		{
 			scanf("%d", &room[i][j]);
 		}
 	}
 
-	for (i = 1; i <= N; i++)
+	for (int i = 1; i <= N; i++)
 	{
-		for (j = 1; j <= M; j++)
+		for (int j = 1; j <= M; j++)
 		{
-			Max = max(max(Dp[i - 1][j], Dp[i][j - 1]), Dp[i - 1][j - 1]);
-			Dp[i][j] = Max + room[i][j];
+			dp[i][j] = max(max(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + room[i][j];
 		}
 	}
 
-	printf("%d\n", Dp[N][M]);
-
+	printf("%d\n", dp[N][M]);
 
 	return 0;
 }
